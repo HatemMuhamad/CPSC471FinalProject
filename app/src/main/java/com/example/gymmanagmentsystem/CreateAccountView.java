0cdbdc29b5ba862243159659e1dcbf7c6fd06148
ColtonOsterlund <coltonericosterlund@gmail.com>
2019-12-05 00:07:07 -0700

    ada

--- app/src/main/java/com/example/gymmanagmentsystem/CreateAccountView.java
+++ app/src/main/java/com/example/gymmanagmentsystem/CreateAccountView.java
@@ -41,13 +41,13 @@ public class CreateAccountView extends AppCompatActivity {
         final TextView provinceTextView = (TextView) findViewById(R.id.provinceTextField);
         final TextView postalTextView = (TextView) findViewById(R.id.postalTextField);
 
-        Button createAccountBtn = (Button) findViewById(R.id.createMemberAccountBtn);
-        createAccountBtn.setOnClickListener(new View.OnClickListener() {
+        Button memberAccountBtn = (Button) findViewById(R.id.createMemberAccountBtn);
+        Button trainerAccountBtn = (Button) findViewById(R.id.createTrainerAccountBtn);
+
+        memberAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)  {
-
-
-//                personGymID = String.valueOf(rand.nextInt(10000));
+            public void onClick(View view) {
+               // personGymID = String.valueOf(rand.nextInt(10000));
                 emergContactNumber = emergContactNoTextView.getText().toString();
                 phoneNumber = userPhoneNoTextView.getText().toString();
                 streetName = streetTextView.getText().toString();
@@ -97,11 +97,10 @@ public class CreateAccountView extends AppCompatActivity {
                 int number = rand.nextInt(10000);
                 personGymID = ((Integer)number).toString();
 
-                addListenerOnSpinnerItemSelection();
                 try{
 
                     int success = GymManagementController.signUp(emergContactNumber, personGymID, phoneNumber, streetName, cityName, provinceName,
-                            postalCode, TFlag, MFlag);
+                            postalCode, 0, 1);
 
                     if(success == 0){
                         Toast.makeText(CreateAccountView.this, "Could Not Create Account",
@@ -134,23 +133,99 @@ public class CreateAccountView extends AppCompatActivity {
                 }
             }
         });
+
+       trainerAccountBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               // personGymID = String.valueOf(rand.nextInt(10000));
+               emergContactNumber = emergContactNoTextView.getText().toString();
+               phoneNumber = userPhoneNoTextView.getText().toString();
+               streetName = streetTextView.getText().toString();
+               cityName = cityTextView.getText().toString();
+               provinceName = provinceTextView.getText().toString();
+               postalCode = postalTextView.getText().toString();
+
+               //TODO Get this to work
+               //FIND A BETTER WAY TO DO THIS - I AM JUST DOING IT THIS WAY FOR NOW. IS THERE SOME ASSERT METHOD/FLAG ON THE TEXTVIEW ITSELF?
+               if (emergContactNumber.isEmpty()) {
+                   Toast.makeText(CreateAccountView.this, "You must supply an Emergency Contact Number",
+                           Toast.LENGTH_SHORT).show();
+               } else if (phoneNumber.isEmpty()) {
+                   Toast.makeText(CreateAccountView.this, "You must supply a Phone Number",
+                           Toast.LENGTH_SHORT).show();
+               } else if (streetName.isEmpty()) {
+                   Toast.makeText(CreateAccountView.this, "You must supply a Street Name",
+                           Toast.LENGTH_SHORT).show();
+               } else if (cityName.isEmpty()) {
+                   Toast.makeText(CreateAccountView.this, "You must supply a City Name",
+                           Toast.LENGTH_SHORT).show();
+               } else if (provinceName.isEmpty()) {
+                   Toast.makeText(CreateAccountView.this, "You must supply a Province Number",
+                           Toast.LENGTH_SHORT).show();
+               } else if (postalCode.isEmpty()) {
+                   Toast.makeText(CreateAccountView.this, "You must supply a Postal Code",
+                           Toast.LENGTH_SHORT).show();
+               }
+
+               //TODO get this to work
+//                SecureRandom random = new SecureRandom();
+//                byte[] salt = new byte[16];
+//                random.nextBytes(salt);
+//
+//                MessageDigest md = null;
+//                try {
+//                    md = MessageDigest.getInstance("SHA-512");
+//                } catch (NoSuchAlgorithmException e) {
+//                    e.printStackTrace();
+//                }
+//                md.update(salt);
+//
+//                byte[] rand = md.digest(phoneNumber.getBytes(StandardCharsets.UTF_8));
+//                String personGymID = rand.toString().substring(0, 8);
+
+               Random rand = new Random();
+               int number = rand.nextInt(10000);
+               personGymID = ((Integer)number).toString();
+
+               try{
+
+                   int success = GymManagementController.signUp(emergContactNumber, personGymID, phoneNumber, streetName, cityName, provinceName,
+                           postalCode, 1, 0);
+
+                   if(success == 0){
+                       Toast.makeText(CreateAccountView.this, "Could Not Create Account",
+                               Toast.LENGTH_SHORT).show();
+
+                       emergContactNoTextView.setText("");
+                       userPhoneNoTextView.setText("");
+                       streetTextView.setText("");
+                       cityTextView.setText("");
+                       provinceTextView.setText("");
+                       postalTextView.setText("");
+
+                   }
+
+                   else if (success == 1){
+                       Toast.makeText(CreateAccountView.this, "Your Gym ID is: " + personGymID,
+                               Toast.LENGTH_LONG).show();
+
+                       emergContactNoTextView.setText("");
+                       userPhoneNoTextView.setText("");
+                       streetTextView.setText("");
+                       cityTextView.setText("");
+                       provinceTextView.setText("");
+                       postalTextView.setText("");
+                   }
+
+
+               }catch(SQLException e){
+                   e.printStackTrace();
+               }
+           }
+       });
+
+
     }
-    private void memberSignupBtnListener() throws SQLException{
-
-            @Override
-            public void onClick(View v) {
-            }
-        };
-
-    private void trainerSignupBtnListener() throws SQLException{
-
-        @Override
-        public void onClick(View v) {
-        }
-    };
-
-
-}
 
 }
 
